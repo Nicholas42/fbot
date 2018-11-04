f4987dd0aa9bf705feffeb6bf1f2df6a290826bb
Franz König <>
2018-11-04 23:01:31 +0100

    added schema

---
+++ fbotdb.sql
@@ -0,0 +1,38 @@
+CREATE TABLE nicknames (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nickname TEXT,
+userid INTEGER,
+deletable INTEGER DEFAULT 0);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE IF NOT EXISTS "pings" (
+	`recipient`	TEXT,
+	`message`	TEXT,
+	`sender`	TEXT,
+	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`messageid`	INTEGER
+);
+CREATE TABLE `freiepunkteliste` (
+	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`name`	TEXT
+);
+CREATE TABLE IF NOT EXISTS "freiepunkte" (
+	`userid`	INTEGER,
+	`freiepunkteid`	INTEGER,
+	`anzahl`	INTEGER
+);
+CREATE TABLE `songs` (
+	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`link`	TEXT
+);
+CREATE TABLE qedpoll (name, num);
+/* completion(candidate) */;
+/* dbstat(name,path,pageno,pagetype,ncell,payload,unused,mx_payload,pgoffset,pgsize) */;
+/* fsdir(name,mode,mtime,data) */;
+/* fts3tokenize(input,token,start,"end",position) */;
+/* json_each("key",value,type,atom,id,parent,fullkey,path) */;
+/* json_tree("key",value,type,atom,id,parent,fullkey,path) */;
+/* pragma_database_list(seq,name,file) */;
+/* pragma_module_list(name) */;
+/* sqlite_dbpage(pgno,data) */;
+/* sqlite_stmt(sql,ncol,ro,busy,nscan,nsort,naidx,nstep,reprep,run,mem) */;
+/* zipfile(name,mode,mtime,sz,rawdata,data,method) */;
