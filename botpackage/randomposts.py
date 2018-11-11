7c6480702d3e271fc77274a76af1edbc151cf8b8
Franz König <>
2018-11-11 17:28:34 +0100

    unsufficient renamed variable

--- botpackage/randomposts.py
+++ botpackage/randomposts.py
@@ -12,7 +12,7 @@ _names = {
 
 
 def processMessage(args, rawMessage, db_connection):
-	if random.randrange(_randompost_propability) == 0:
+	if random.randrange(_randompost_one_over_probability) == 0:
 		msg_obj = _names[random.choice(list(_names))]
 		return helper.botMessage(msg_obj[0], msg_obj[1])
 	return
