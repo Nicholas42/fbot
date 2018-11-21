1d09aad13f55796fc3da91fe8fc8db6e56fcc8ea
Franz König <>
2018-11-21 16:50:23 +0100

    praise the Jana \o/

--- botpackage/randomposts.py
+++ botpackage/randomposts.py
@@ -3,16 +3,17 @@ import random
 from botpackage.helper import helper
 
 _randompost_one_over_probability = 500
-_names = {
-	'manf':['Hack the planet!','Paladschinken'],
-	'luise':['Lalalalalalalalalalala','Luise'],
-	'kathie':['Ich bin voll putzig. Du auch.','Kathie'],
-	'nicholas':['Knuddeln ist auch ein Sport. *knuddelt*','Nicholas']
-}
+_names = [
+	['Hack the planet!','Paladschinken'],
+	['Lalalalalalalalalalala','Luise'],
+	['Ich bin voll putzig. Du auch.','Kathie'],
+	['Knuddeln ist auch ein Sport. *knuddelt*','Nicholas'],
+	['Praise the Jana \o/', '\t \t  \t    \t\t\t  \t    \t Jörn']
+]
 
 
 def processMessage(args, rawMessage, db_connection):
 	if random.randrange(_randompost_one_over_probability) == 0:
-		msg_obj = _names[random.choice(list(_names))]
+		msg_obj = random.choice(_names)
 		return helper.botMessage(msg_obj[0], msg_obj[1])
 	return
