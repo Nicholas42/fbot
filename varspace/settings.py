71d141ea896a5788e98978e734571a30678b2597
Franz König <>
2019-01-17 23:31:05 +0100

    settings.py: adding new variables

--- varspace/settings.py
+++ varspace/settings.py
@@ -1,7 +1,11 @@
+botMasters = ['FranzKönig']
+
 credentials = {
-		'username' : 'FranzBots',
-		'password' : '',
-		'version' : '20171030131648'
+    'username' : 'FranzBots',
+    'password' : '',
+    'version' : '20171030131648'
 }
+google_api_key = ''
 
 _time_between_botposts = 2 # in seconds
+overwrite_pings = False
